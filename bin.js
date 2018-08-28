aa2d9e6fd2eee573737bcbcaa60afb0d7e53eaac
Fernando Migliorini Tenório <dotenorio@gmail.com>
2018-08-28 19:01:49 -0300

    feat: able to require in your project

--- bin.js
+++ bin.js
@@ -1,12 +1,3 @@
 #!/usr/bin/env node
 
-console.log(JSON.stringify({
-  name: 'Fernando M Tenório',
-  github: 'https://github.com/dotenorio',
-  twitter: 'https://twitter.com/dotenorio',
-  linkedin: false,
-  facebook: 'https://www.facebook.com/dotenorio',
-  telegram: 'https://telegram.me/dotenorio',
-  email: 'dotenorio@gmail.com',
-  blog: 'http://dotenorio.github.io'
-}, null, 2))
+console.log(JSON.stringify(require('./index')(), null, 2))
---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+module.exports = () => ({
+  name: 'Fernando M Tenório',
+  github: 'https://github.com/dotenorio',
+  twitter: 'https://twitter.com/dotenorio',
+  linkedin: false,
+  facebook: 'https://www.facebook.com/dotenorio',
+  telegram: 'https://telegram.me/dotenorio',
+  email: 'dotenorio@gmail.com',
+  blog: 'http://dotenorio.github.io'
+})
