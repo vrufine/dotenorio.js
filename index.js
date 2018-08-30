f77b6ddc5ed7f70ed51fa350359b40ed37891af9
Fernando Milgiorini Tenório <dotenorio@gmail.com>
2018-08-30 13:27:52 -0300

    fix: add bio and picture

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 module.exports = () => ({
   name: 'Fernando M Tenório',
+  picture: 'https://www.gravatar.com/avatar/9c62a4730ba4ebf764862b7866038dcb?s=300',
+  bio: 'Estou na área de TI a mais de 10 anos, sempre trabalhei com tecnologias web. No momento, atuo como engenheiro, procurando formas de tornar a empresa melhor.',
   github: 'https://github.com/dotenorio',
   twitter: 'https://twitter.com/dotenorio',
   linkedin: false,
